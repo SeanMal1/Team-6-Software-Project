cc22d1257c52871f4e4922000a823275b046747f
mark-marron <markmarron16@gmail.com>
2023-01-25 19:20:50 +0000

    player pos and sprite location now read from save file

--- code/world.py
+++ code/world.py
@@ -1,16 +1,18 @@
 import pygame
+import json
 from settings import *
 from player import Player
 
 class Level:
     def __init__(self):
+        self._saveFile = json.load(open("profiles/save1.json", "r"))
         self._DisplayWorld = pygame.display.get_surface()
         self._AllSprites = pygame.sprite.Group()
         self.setup()
-        self._SpriteSheetImage = pygame.image.load('textures/player.png').convert_alpha()
+        self._SpriteSheetImage = pygame.image.load(self._saveFile['image']).convert_alpha()
 
     def setup(self):
-        self._Player = Player((640,360), self._AllSprites)
+        self._Player = Player((self._saveFile['position']['x'], self._saveFile['position']['y']), self._AllSprites)
 
     def run(self,DeltaTime):
         self._DisplayWorld.fill('black')
