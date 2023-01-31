80ed070048d7e6229f5eacaaaf83e44101ef77b9
Joseph O'Donovan <118483276@umail.ucc.ie>
2023-01-31 00:00:59 +0000

    Added all files for map, textures and map image

---
+++ data/Tilesets/Grass hill tiles v.2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Grass hill tiles v.2" tilewidth="16" tileheight="16" tilecount="77" columns="11">
+ <image source="Grass hill tiles v.2.png" width="176" height="112"/>
+</tileset>
---
+++ data/Tilesets/objects.tsx
@@ -0,0 +1,106 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="objects" tilewidth="32" tileheight="31" tilecount="34" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="10" height="8" source="../objects/033.png"/>
+ </tile>
+ <tile id="1">
+  <image width="24" height="31" source="../objects/000.png"/>
+ </tile>
+ <tile id="2">
+  <image width="14" height="28" source="../objects/001.png"/>
+ </tile>
+ <tile id="3">
+  <image width="24" height="31" source="../objects/002.png"/>
+ </tile>
+ <tile id="4">
+  <image width="13" height="13" source="../objects/003.png"/>
+ </tile>
+ <tile id="5">
+  <image width="10" height="11" source="../objects/004.png"/>
+ </tile>
+ <tile id="6">
+  <image width="9" height="9" source="../objects/005.png"/>
+ </tile>
+ <tile id="7">
+  <image width="14" height="12" source="../objects/006.png"/>
+ </tile>
+ <tile id="8">
+  <image width="8" height="5" source="../objects/007.png"/>
+ </tile>
+ <tile id="9">
+  <image width="13" height="12" source="../objects/008.png"/>
+ </tile>
+ <tile id="10">
+  <image width="16" height="15" source="../objects/009.png"/>
+ </tile>
+ <tile id="11">
+  <image width="13" height="9" source="../objects/010.png"/>
+ </tile>
+ <tile id="12">
+  <image width="10" height="8" source="../objects/011.png"/>
+ </tile>
+ <tile id="13">
+  <image width="16" height="12" source="../objects/012.png"/>
+ </tile>
+ <tile id="14">
+  <image width="32" height="16" source="../objects/013.png"/>
+ </tile>
+ <tile id="15">
+  <image width="30" height="12" source="../objects/014.png"/>
+ </tile>
+ <tile id="16">
+  <image width="16" height="14" source="../objects/015.png"/>
+ </tile>
+ <tile id="17">
+  <image width="6" height="6" source="../objects/016.png"/>
+ </tile>
+ <tile id="18">
+  <image width="6" height="6" source="../objects/017.png"/>
+ </tile>
+ <tile id="19">
+  <image width="16" height="16" source="../objects/018.png"/>
+ </tile>
+ <tile id="20">
+  <image width="16" height="16" source="../objects/019.png"/>
+ </tile>
+ <tile id="21">
+  <image width="11" height="9" source="../objects/020.png"/>
+ </tile>
+ <tile id="22">
+  <image width="9" height="6" source="../objects/021.png"/>
+ </tile>
+ <tile id="23">
+  <image width="11" height="12" source="../objects/022.png"/>
+ </tile>
+ <tile id="24">
+  <image width="14" height="29" source="../objects/023.png"/>
+ </tile>
+ <tile id="25">
+  <image width="11" height="9" source="../objects/024.png"/>
+ </tile>
+ <tile id="26">
+  <image width="9" height="6" source="../objects/025.png"/>
+ </tile>
+ <tile id="27">
+  <image width="16" height="10" source="../objects/026.png"/>
+ </tile>
+ <tile id="28">
+  <image width="10" height="10" source="../objects/027.png"/>
+ </tile>
+ <tile id="29">
+  <image width="8" height="10" source="../objects/028.png"/>
+ </tile>
+ <tile id="30">
+  <image width="13" height="14" source="../objects/029.png"/>
+ </tile>
+ <tile id="31">
+  <image width="7" height="8" source="../objects/030.png"/>
+ </tile>
+ <tile id="32">
+  <image width="7" height="5" source="../objects/031.png"/>
+ </tile>
+ <tile id="33">
+  <image width="16" height="12" source="../objects/032.png"/>
+ </tile>
+</tileset>
