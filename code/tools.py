4525ef30fdd0a12e6e9e8db37f8ba72d61876d75
Joseph O'Donovan <118483276@umail.ucc.ie>
2023-02-01 23:15:56 +0000

    Added scaling to import_folder function to fix scaling on water images and future images

--- code/tools.py
+++ code/tools.py
@@ -1,14 +1,17 @@
 from os import walk
 import pygame
+from sprites import Generic
 
 # Function to import images in folder and convert them into surfaces
 def import_folder(path):
     surface_list = []
+    scale = 3
 
     for _, __, images in walk(path):
         for image in images:
             full_path = path + '/' + image
             image_surface = pygame.image.load(full_path).convert_alpha()
+            image_surface = pygame.transform.scale(image_surface, (image_surface.get_width() * scale, image_surface.get_height() * scale))
             surface_list.append(image_surface)
 
     return surface_list
