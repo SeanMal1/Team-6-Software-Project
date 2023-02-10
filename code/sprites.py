430e5b98460db150ca155ecc76823a76d80b6686
SeanMal1 <120354053@umail.ucc.ie>
2023-02-10 17:13:30 +0000

    Added random plums to trees
    used a plum texture as well as positions for the plums to spawn in settings.py and a spawn rate of 20% in trees. The plan is that these plums will fall out of the tree when the tree is hit

--- code/sprites.py
+++ code/sprites.py
@@ -1,5 +1,6 @@
 import pygame
 from settings import *
+from random import randint
 
 
 class Generic(pygame.sprite.Sprite):
@@ -51,3 +52,16 @@ class Tree(Generic):
     def __init__(self, pos, surface, groups, name):  # name is for the type of tree, e.g small, large
         super().__init__(pos, surface, groups)
         self.hitbox = self.rect.copy().inflate(-self.rect.height * 0.4, -self.rect.height * 0.25)
+
+        #plum
+        self._PlumSurface = pygame.image.load('../textures/misc/plum.png')
+        self._PlumPosition = PlumPos['Large']
+        self._PlumSprites = pygame.sprite.Group()
+        self.CreatePlum()
+
+    def CreatePlum(self):
+        for pos in self._PlumPosition:
+            if randint(0,10) < 2:
+                x = pos[0] + self.rect.left
+                y = pos[1] + self.rect.top
+                Generic((x,y),self._PlumSurface,[self._PlumSprites,self.groups()[0]],z= LAYERS['fruit'])
