1ea15b8bc4d672ec70541f51316c10fc61ea5445
Joseph O'Donovan <118483276@umail.ucc.ie>
2023-02-16 20:26:50 +0000

    updated map, added bed and trader objects for interaction, decorated house map

--- data/Tilesets/Furniture.tsx
+++ data/Tilesets/Furniture.tsx
@@ -1,2 +1,43 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Furniture" tilewidth="1" tileheight="1" tilecount="0" columns="0"/>
+<tileset version="1.9" tiledversion="1.9.2" name="Furniture" tilewidth="24" tileheight="22" tilecount="13" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="14" height="22" source="../objects/furniture/bed_blue.png"/>
+ </tile>
+ <tile id="1">
+  <image width="14" height="22" source="../objects/furniture/bed_green.png"/>
+ </tile>
+ <tile id="2">
+  <image width="14" height="22" source="../objects/furniture/bed_pink.png"/>
+ </tile>
+ <tile id="3">
+  <image width="10" height="13" source="../objects/furniture/chair_left.png"/>
+ </tile>
+ <tile id="4">
+  <image width="10" height="13" source="../objects/furniture/chair_right.png"/>
+ </tile>
+ <tile id="5">
+  <image width="9" height="9" source="../objects/furniture/clock_wall.png"/>
+ </tile>
+ <tile id="6">
+  <image width="12" height="8" source="../objects/furniture/painting.png"/>
+ </tile>
+ <tile id="7">
+  <image width="24" height="13" source="../objects/furniture/rug_large_blue.png"/>
+ </tile>
+ <tile id="8">
+  <image width="24" height="13" source="../objects/furniture/rug_large_green.png"/>
+ </tile>
+ <tile id="9">
+  <image width="24" height="13" source="../objects/furniture/rug_large_pink.png"/>
+ </tile>
+ <tile id="10">
+  <image width="16" height="13" source="../objects/furniture/rug_small_blue.png"/>
+ </tile>
+ <tile id="11">
+  <image width="16" height="13" source="../objects/furniture/rug_small_green.png"/>
+ </tile>
+ <tile id="12">
+  <image width="16" height="13" source="../objects/furniture/rug_small_pink.png"/>
+ </tile>
+</tileset>
