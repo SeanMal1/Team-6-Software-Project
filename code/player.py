86623c0cf1f3143eec62ae85881263e9b7f55a5c
Eoin O'Sullivan <74117793+EoinOSullivanCS@users.noreply.github.com>
2023-02-28 12:13:46 +0000

    Fix Merchant and Inventory bug

--- code/player.py
+++ code/player.py
@@ -276,10 +276,8 @@ class Player(pygame.sprite.Sprite):
                 if self._prevKeystroke[pygame.K_u] and not keystroke[pygame.K_u]:
                     self.shop_active = not self.shop_active
                     if _CollidedInteractionSprite:
-                        if not self.timer['enter shop']._Active:
                             if _CollidedInteractionSprite[0].name == 'Trader':
-                                self.timer['enter shop'].activate()
-                                self.shop_active = not self.shop_active
+                                
                                 self.toggle_merchant()
 
             self._prevKeystroke = keystroke
