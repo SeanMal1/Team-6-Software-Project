f4df197fe8576880f8f407e92b7476d50e941c56
Joseph <118483276@umail.ucc.ie>
2023-03-08 15:17:55 +0000

    Updated overlay rect boarde radius to be consistent with other UI

--- code/overlay.py
+++ code/overlay.py
@@ -14,15 +14,15 @@ class Overlay:
         self._SeedsOverlay = {seed: pygame.image.load(f'{OverlayPath}{seed}.png').convert_alpha() for seed in player._Seeds}
 
     def display_tools(self):
-        pygame.draw.rect(self._DisplaySurface, (255, 255, 255, 0), pygame.Rect(5, ScreenHeight-80, 260, 70), border_radius=20)
+        pygame.draw.rect(self._DisplaySurface, (255, 255, 255, 0), pygame.Rect(5, ScreenHeight-80, 260, 70), border_radius=4)
         
         for (i, tool) in enumerate(self._Player._Tools):
             self._DisplaySurface.blit(pygame.transform.scale(self._ToolsOverlay[tool], (48, 48)), (20 + 60*i, ScreenHeight-70))
             if tool == self._Player._SelectedTool:
-                pygame.draw.rect(self._DisplaySurface, (255, 0, 0), pygame.Rect(15 + 60*i, ScreenHeight-75, 58, 58), width=2, border_radius=10)
+                pygame.draw.rect(self._DisplaySurface, (255, 0, 0), pygame.Rect(15 + 60*i, ScreenHeight-75, 58, 58), width=2, border_radius=4)
 
     def display_seed(self):
-        pygame.draw.rect(self._DisplaySurface, (255, 255, 255, 0), pygame.Rect(ScreenWidth-80, ScreenHeight-80, 70, 70), border_radius=20)
+        pygame.draw.rect(self._DisplaySurface, (255, 255, 255, 0), pygame.Rect(ScreenWidth-80, ScreenHeight-80, 70, 70), border_radius=4)
         self._DisplaySurface.blit(pygame.transform.scale(self._SeedsOverlay[self._Player._SelectedSeed], (48, 48)), (ScreenWidth-75, ScreenHeight-65))
 
     def display_health(self):
